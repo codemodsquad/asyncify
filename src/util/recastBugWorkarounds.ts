9a401ed46b8f63dd66658fd5fc8e3f108a34abd0
Andy Edwards <jedwards@fastmail.com>
2020-04-19 23:01:56 -0500

    fix(recastBugWorkarounds): rework how comments are handled

--- src/util/recastBugWorkarounds.ts
+++ src/util/recastBugWorkarounds.ts
@@ -2,10 +2,27 @@ import * as t from '@babel/types'
 import { NodePath } from '@babel/traverse'
 
 export default function recastBugWorkarounds(path: NodePath<any>): void {
+  const visitedComments: Set<t.Comment> = new Set()
   path.traverse({
     exit(path: NodePath<t.Node>) {
+      const { leadingComments } = path.node
       const anyNode = path.node as any
-      anyNode.comments = anyNode.leadingComments
+      if (leadingComments) {
+        anyNode.comments = []
+        if (leadingComments) {
+          for (const comment of leadingComments) {
+            if (visitedComments.has(comment)) continue
+            visitedComments.add(comment)
+            anyNode.comments.push({
+              ...comment,
+              leading: true,
+              trailing: false,
+            })
+          }
+        }
+      } else {
+        anyNode.comments = null
+      }
     },
     AwaitExpression(path: NodePath<t.AwaitExpression>) {
       const argument = path.get('argument')
@@ -15,6 +32,24 @@ export default function recastBugWorkarounds(path: NodePath<any>): void {
         (parentPath.isMemberExpression() && path === parentPath.get('object'))
       ) {
         argument.replaceWith(t.parenthesizedExpression(argument.node))
+      }
+    },
+  })
+  path.traverse({
+    exit(path: NodePath<t.Node>) {
+      const { trailingComments } = path.node
+      const anyNode = path.node as any
+      if (trailingComments) {
+        if (!anyNode.comments) anyNode.comments = []
+        for (const comment of trailingComments) {
+          if (visitedComments.has(comment)) continue
+          visitedComments.add(comment)
+          anyNode.comments.push({
+            ...comment,
+            leading: false,
+            trailing: true,
+          })
+        }
       }
     },
   })
